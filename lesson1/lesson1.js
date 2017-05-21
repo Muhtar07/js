3f0c3b6f6e3a41e5c0898525209b28f761c9f5c1
Muhtar07 <bmuhtar07@gmail.com>
2017-05-21 23:45:00 +0300

    ДЗ на 1 урок

---
+++ lesson1/lesson1.js
@@ -0,0 +1,96 @@
+//1
+var Tc=35;
+var Tf=(9/5)*Tc+32;
+alert(Tc +" градусов Цельсия равна " + Tf +" градусам Фаренгейта ");
+//2
+var name="Вассилий";
+var admin=name;
+alert(admin);
+//3
+alert(1000+"108");
+//*
+var nameSite = "liberty-shop.ru";//переменная с именем сайта
+
+var menu=["Главная","Контакты","Продукты","Ценны" ];//Переменная-массив для меню сайта
+
+var articleOnePage=55;//Количество статие на странице
+
+var topic;//Переменная c зоголовком статьи
+
+var articleText;//Переменная для обычных статей
+
+var importantArticle;//Переменная для важных статей
+
+var humorousArticle;//Переменная для юмористических статей
+
+var seriousArticle;//Переменная из блока "а если а серьезном"
+
+var articleimage=true;//логическая переменная
+
+var price=100;//ценаа товара 
+
+var sad = "This is Sparta!!!";
+
+var result = true;  //Значение есть результат или нет. Если есть true, если значения нет  
+                    // false
+var entage = 20;
+
+var header = "Заголовок новой статьи";
+
+var zipCode = "983456";
+
+prompt("Введите хоть что-нибудь");
+
+alert ("Вы точно все ввели?");
+
+var var1 = 25;
+
+var var2 = " Орехов";
+
+alert ("Вы нашли "+var1+var2);
+var var3 = 45;
+var var4 = 20;
+
+alert (var3+var4);   // Этот результат проверить. Должно получиться 65.
+
+var art ="25/5";//string
+
+var smae =null;//object
+
+var scoop;//undefined
+
+var flag = 0;//number
+
+var plat=true;//boolean
+
+var devid = 34 / 7;//number
+var person = {lastname: Ivan};//undefined
+var fruits = ['apple'];//object
+var iceCream = function() { alert('Я люблю мороженое'); }//"undefined"
+
+var str1="“JavaScript is a high-level, dynamic, untyped, and interpreted programming language”."//83
+var str2="“Netscape Communications realized that the Web needed to become more dynamic”."//78
+var str3="“JavaScript was realized in ten days”."//38
+
+var a="console.log";
+var b="Hello World!";
+var c="I need more code on JavaScript";
+
+
+var a1=a.toUpperCase();
+
+var b1=b.toUpperCase();
+
+var c1=c.toUpperCase();
+
+var a2=a1.toLowerCase();
+
+var b2=b1.toLowerCase();
+
+var c2=c1.toLowerCase();
+
+var a3=a [0].toUpperCase();
+
+var b3=b [0].toUpperCase();
+
+var c3=c [0].toUpperCase();
